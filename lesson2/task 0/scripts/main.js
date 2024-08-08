7d44d8806bdf54f61606f60da881027caae2c1a6
Vladyslav <78151203+kanderyabl@users.noreply.github.com>
2024-08-08 08:33:49 +0200

    Add files via upload

---
+++ lesson2/task 0/scripts/main.js
@@ -0,0 +1,8 @@
+// let a = parseInt(prompt("вкажіть значення а:", "1"));
+// let b = parseInt(prompt("вкажіть значення b:", "1"));
+// let c = parseInt(prompt("вкажіть значення c:", "1"));
+// const s1 = document.querySelector("S1");
+// let S1 = a + 12 + b;
+// s1.addEventListener("input", () => {
+//   s1.innerHTML = "S1 = " + S1;
+// });
